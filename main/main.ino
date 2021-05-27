cec2fca3f1c6eb575324db688a83424e3a0ac96f
Lupindakaas <lucasbdekker@hotmail.nl>
2021-05-26 22:57:24 -0700

    updated stuff

--- main/main.ino
+++ main/main.ino
@@ -10,20 +10,20 @@
  */
 
 //------------------------------------------------------------------//
-//                   define/ include's                              //
+//                   define/ include/ prototypes                    //
 //------------------------------------------------------------------//
 #define UNIT 1000 // one unit of time in Morse
 #define BN 2 // the pin the button is connected to
 
 
-
+void process(struct letter currentL)
 
 //------------------------------------------------------------------//
 //                   All keyboard character structs                 //
 //------------------------------------------------------------------//
 /*
 struct letter{
-  char letter;
+  char character;
   struct letter *next;
   
 } a,b,c,d,e,f,g,h,i,j,k,l,m,n,o,p,q,r,s,t,x,y,z;
@@ -33,16 +33,15 @@ struct letter{
 //------------------------------------------------------------------//
 
 void setup() {
-  // put your setup code here, to run once:
   pinMode(BN, INPUT_PULLUP);
   
 
 }
 
 void loop() {
-  // put your main code here, to run repeatedly:
+  
   if (digitalRead(BN)){
-    //e();
+    process();
   }
 }
 
@@ -54,29 +53,7 @@ void loop() {
 //------------------------------------------------------------------//
 
 
-void charOne() {
-  /* precondition: the botton was clicked(1)
-   *  first we have to wait a unit
-   *  now that the unit is over, we can check the button again,
-   *  
-   *  if BN is 1{ 
-   *    since the button has been pushed down 2 units, the letter starts with a dash.
-   *    these take however take 3 units, so we wait another unit.
-   *    
-   *    now that 3 units are over since the begin of the button press, the button should be 0.
-   *    IF BN is 1
-   *      
-   *    ELSE IF bn is 0
-   *      
-   *  
-   *  }
-   *  
-   *  ELSE IF bn is 0 
-   *    the letter starts with a 
-   *  
-   *  
-   */
-
+void funcEnT(struct letter currentL) {
   // first we have to wait out the first unit.
   delay(UNIT); // unit 1
 
@@ -93,29 +70,28 @@ void charOne() {
       keyboard.write('t');
       keyboard.write(' ');
     } else {
-      
+      //TODO
     }
     
   } else {
     // if the button isn't pressed down anymore after unit 1
     // we have to wait 1 unit, to see if the character has ended, or there is more to come
-    delay(UNIT);
+    delay(UNIT); // unit 2
     
     if (digitalRead(BN)) {
       // it seems there is more to come, considering we have to start at a morse dot again, we go to the i()[. .   ] function.
       // this part is entered when the letter has another dot or dash, thus we enter the next letter's process.
-      
+      //TODO
       i();
       break;
       
     }else {
       //this bit is a dot.
-      delay(UNIT);
+      delay(UNIT); // unit 3
       // if the button isn't pressed down in units 2,3 and 4, the character has ended.
       // this means we can write the current char to the computer.
       keyboard.write('e');
       keyboard.write(' ');
     }
   }
-  
 }
